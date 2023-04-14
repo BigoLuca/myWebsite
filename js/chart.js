fbc8795b7a6747ec02fec594964d7290ff039bd1
Luca Bighini <bighini.luca@gmail.com>
2023-04-14 21:40:12 +0200

    Add tab chart

---
+++ js/chart.js
@@ -0,0 +1,12 @@
+function openCity(evt, cityName) {
+    var i, tablinks, tabcontent;
+    tablinks = document.getElementsByClassName("tablinks");
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
