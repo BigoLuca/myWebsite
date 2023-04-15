aac3873f9c2f8781421cf18afa7dc59a9fea8a0f
Luca Bighini <bighini.luca@gmail.com>
2023-04-15 14:14:55 +0200

    Chek login

--- js/login.js
+++ js/login.js
@@ -1,8 +1,20 @@
+let users = [];
 let req = new XMLHttpRequest();
 req.open('GET', './user.json');
 req.responseType = 'json';
 req.send();
 req.onload = function () {
-    const user = req.response;
-    console.log(user);
+    users = req.response;
+    console.log(users);
 }
+
+const sb_btn = document.querySelector('.submit_btn').addEventListener('click', function(){
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    users.forEach(e => {
+        if(e["Email"] === email && e["Password"] === password){
+            console.log("ok");
+            window.location.replace("https://bigoluca.github.io/myWebsite/");
+        }
+    });
+});
